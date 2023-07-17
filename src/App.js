6853a532cd14a6e5d0537b3786773684378cc0e2
Raymond Fajiculay <raymondfajiculay@Raymond.local>
2023-07-17 22:46:08 +0800

    add fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from "./MovieCard";
 import "./App.css"
 import SearchIcon from "./search.svg"
 
-const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
+const API_URL = `https://www.omdbapi.com?apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
 
 const App = () => {
     const [movies, setMovies] = useState([])
